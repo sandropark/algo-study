f0ec59c4cdcf3e3b4cb3846221d992672e08a172
sandropark <ticoba@naver.com>
2024-09-19 23:27:10 +0900

    [12장 그래프] 테스트 코드 구조 잡기

---
+++ 12장 그래프/src/main/java/com/algo/graph/Graph.java
@@ -0,0 +1,22 @@
+package com.algo.graph;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+public class Graph {
+    private Map<Integer, List<Integer>> graph;
+
+    public List<Integer> bfs(int vertex) {
+        return null;
+    }
+
+    public void dfsRecursive(int vertex, List<Integer> result) {
+    }
+
+    public List<Integer> dfsIterative(int vertex) {
+        return null;
+    }
+}
---
+++ 12장 그래프/src/test/java/com/algo/graph/GraphTest.java
@@ -0,0 +1,46 @@
+package com.algo.graph;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GraphTest {
+    Map<Integer, List<Integer>> graphMap = Map.of(
+            1, List.of(2, 3, 4),
+            2, List.of(5),
+            3, List.of(5),
+            4, List.of(),
+            5, List.of(6, 7),
+            6, List.of(),
+            7, List.of(3)
+    );
+
+    Graph graph = new Graph(graphMap);
+
+    @Test
+    void dfsRecursive() throws Exception {
+        List<Integer> result = new ArrayList<>();
+
+        graph.dfsRecursive(1, result);
+
+        assertThat(result).containsExactly(1, 2, 5, 6, 7, 3, 4);
+    }
+
+    @Test
+    void dfsIterative() throws Exception {
+        List<Integer> result = graph.dfsIterative(1);
+
+        assertThat(result).containsExactly(1, 2, 5, 6, 7, 3, 4);
+    }
+
+    @Test
+    void bfs() throws Exception {
+        List<Integer> result = graph.bfs(1);
+
+        assertThat(result).containsExactly(1, 4, 3, 5, 7, 6, 2);
+    }
+}
