9ce35c4dd4dc37b949d14f76a00af8c80d942cbf
sandropark <birdob@gmail.com>
2024-09-24 08:26:27 +0900

    [12장 그래프] [35 - 섬의 개수] 주석 추가

--- 12장 그래프/src/main/java/com/algo/graph/NumberOfIsland_35.java
+++ 12장 그래프/src/main/java/com/algo/graph/NumberOfIsland_35.java
@@ -12,15 +12,20 @@ public class NumberOfIsland_35 {
         int[] x = {0, 0, -1, 1};
         int[] y = {-1, 1, 0, 0};
 
+        // 1. 그래프 전체를 순회
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                // 2. 섬('1')이면 stack에 넣고, 방문표시
                 if (grid[i][j] == '1') {
                     stack.add(new Location(i, j));
-                    grid[i][j] = 'v';
-                    count++;
+                    grid[i][j] = 'v'; // 방문 표시
+                    count++; // 섬 개수 증가
+
+                    // 한 덩이의 섬을 찾을 때까지 반복.
                     while (!stack.isEmpty()) {
                         Location temp = stack.pop();
 
+                        // 하나의 육지에서 동서남북으로 땅인지 확인해서 땅이면 stack에 넣는다.
                         for (int k = 0; k < 4; k++) {
                             int a = temp.x + x[k];
                             int b = temp.y + y[k];
