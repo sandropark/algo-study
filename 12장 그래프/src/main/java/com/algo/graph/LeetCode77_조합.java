b2cf9e1099ce5d32a1d83b1dcb1f9e2912595dbc
sandropark <birdob@gmail.com>
2024-09-24 09:18:39 +0900

    [12장 그래프] [77 - 조합] 속도 최적화 시도
    - 17(87.04%) -> 12(99.57%)

--- 12장 그래프/src/main/java/com/algo/graph/LeetCode77_조합.java
+++ 12장 그래프/src/main/java/com/algo/graph/LeetCode77_조합.java
@@ -6,21 +6,26 @@ import java.util.List;
 public class LeetCode77_조합 {
     public static List<List<Integer>> solution(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        dfs(n, k, 1, path, result);
+        dfs(n, k, 1, new int[k], result);
         return result;
     }
 
-    private static void dfs(int n, int k, int start, List<Integer> path, List<List<Integer>> result) {
+    private static void dfs(int n, int k, int start, int[] path, List<List<Integer>> result) {
         if (k == 0) {
-            result.add(new ArrayList<>(path));
+            extracted(path, result);
             return;
         }
 
         for (int i = start; i <= n; i++) {
-            path.add(i);
+            path[path.length - k] = i;
             dfs(n, k - 1, i + 1, path, result);
-            path.remove(path.size() - 1);
         }
+    }
+
+    private static void extracted(int[] path, List<List<Integer>> result) {
+        List<Integer> list = new ArrayList<>(path.length);
+        for (int e : path)
+            list.add(e);
+        result.add(list);
     }
 }
