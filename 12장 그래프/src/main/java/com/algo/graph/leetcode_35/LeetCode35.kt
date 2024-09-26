00c6ba1e71013d7753b6d768a1f992c098e3c101
sandropark <birdob@gmail.com>
2024-09-26 09:31:32 +0900

    [12장 그래프] [35 섬의 개수] 3차 코틀린 풀이 - 최적화
    - 205ms(83.83%), 44.84mb(86.22%)

--- 12장 그래프/src/main/java/com/algo/graph/leetcode_35/LeetCode35.kt
+++ 12장 그래프/src/main/java/com/algo/graph/leetcode_35/LeetCode35.kt
@@ -4,8 +4,7 @@ fun solution(grid: Array<CharArray>): Int {
     var count = 0
 
     fun dfs(rowIndex: Int, colIndex: Int): Boolean {
-        if (rowIndex < 0 || rowIndex >= grid.size || colIndex < 0 || colIndex >= grid[0].size) return false
-        if (grid[rowIndex][colIndex] != '1') return false
+        if (rowIndex < 0 || rowIndex >= grid.size || colIndex < 0 || colIndex >= grid[0].size || grid[rowIndex][colIndex] != '1') return false
         grid[rowIndex][colIndex] = 'v'
         dfs(rowIndex - 1, colIndex)
         dfs(rowIndex + 1, colIndex)
