66036d9c94c6bb106ac8becc31c81370df502ec0
sandropark <birdob@gmail.com>
2024-09-27 09:42:44 +0900

    [프로그래머스 - 괄호변환] - 1차 풀이

--- work/src/main/java/com/algo/Prog_괄호변환.java
+++ work/src/main/java/com/algo/Prog_괄호변환.java
@@ -1,9 +1,53 @@
 package com.algo;
 
 public class Prog_괄호변환 {
-
     public String solution(String p) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        rec(p, sb);
+        return sb.toString();
+    }
+
+    private static void rec(String s, StringBuilder sb) {
+        if (s.isEmpty()) return;
+        int openCount = 0;
+        int closeCount = 0;
+
+        // 1. 문자열 순회
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 2. 앞,뒤 개수 세기
+            if (c == '(') openCount++;
+            if (c == ')') closeCount++;
+            // 3. 앞,뒤 개수가 같아지면 문자열 분리
+            if (openCount == closeCount) {
+                String u = s.substring(0, i + 1);
+                if (isCorrect(u)) {
+                    sb.append(u);
+                    rec(s.substring(i + 1), sb);
+                } else {
+                    sb.append("(");
+                    rec(s.substring(i + 1), sb);
+                    sb.append(")").append(processU(u));
+                }
+                // u, w 분리 후에는 재귀호출하면 되고 더 이상 반복문을 진행하면 안되기 때문에 반환.
+                return;
+            }
+        }
+    }
+
+    private static boolean isCorrect(String s) {
+        return s.startsWith("(");
+    }
+
+    public static String processU(String s) {
+        if (s.length() == 2) return "";
+        return reverse(s.substring(1, s.length() - 1));
+    }
+
+    private static String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray())
+            sb.append(c == '(' ? ')' : '(');
+        return sb.toString();
     }
 }
--- work/src/test/java/com/algo/Prog_괄호변환Test.java
+++ work/src/test/java/com/algo/Prog_괄호변환Test.java
@@ -1,6 +1,11 @@
 package com.algo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,5 +45,32 @@ class Prog_괄호변환Test {
 
         // Then
         assertThat(result).isEqualTo("()(())()");
+    }
+
+    @Test
+    void test4() throws Exception {
+        // Given
+        String p = "))((";
+
+        // When
+        String result = new Prog_괄호변환().solution(p);
+
+        // Then
+        assertThat(result).isEqualTo("()()");
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void processU(String input, String expected) throws Exception {
+        String result = Prog_괄호변환.processU(input);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> processU() {
+        return Stream.of(
+                Arguments.of(")(", ""),
+                Arguments.of("))((", "()")
+        );
     }
 }
