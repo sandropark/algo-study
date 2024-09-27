c829ccce10076cf3b470e68f3dc6f89a5a1cf1f7
sandropark <birdob@gmail.com>
2024-09-27 10:10:52 +0900

    [프로그래머스 - 괄호변환] - 코틀린 풀이 2

--- work/src/main/java/com/algo/Prog_괄호변환.kt
+++ work/src/main/java/com/algo/Prog_괄호변환.kt
@@ -1,44 +1,35 @@
 package com.algo
 
 fun solution(p: String): String {
-    val sb: StringBuilder = StringBuilder()
+    if (p.isEmpty()) return p
 
-    fun rec(s: String) {
-        if (s.isEmpty()) return
-        var openCount = 0
-        var closedCount = 0
+    var (openCount, closeCount) = 0 to 0
 
-        for (i in s.indices) {
-            val c = s[i]
-            if (c == '(') openCount++
-            if (c == ')') closedCount++
+    for (i in p.indices) {
+        when (p[i]) {
+            '(' -> openCount++
+            ')' -> closeCount++
+        }
+        if (openCount == closeCount) {
+            val u = p.substring(0, i + 1)
+            val w = p.substring(i + 1)
 
-            when {
-                (openCount == closedCount) -> {
-                    val u = s.substring(0, i + 1)
-                    val v = s.substring(i + 1)
-
-                    if (u.startsWith('(')) {
-                        sb.append(u)
-                        rec(v)
-                        return
-                    }
-
-                    sb.append('(')
-                    rec(v)
-                    sb.append(')').append(processU(u))
-                    return
-                }
-            }
+            return if (isCorrect(u))
+                u + solution(w)
+            else
+                "(${solution(w)})${processU(u)}"
         }
     }
+    return ""
+}
 
-    rec(p)
-    return sb.toString()
+fun isCorrect(s: String): Boolean {
+    return s.startsWith("(")
 }
 
 fun processU(s: String): String {
-    return if (s.length == 2) "" else reverse(s.substring(1, s.length - 1))
+    if (s.length == 2) return ""
+    return reverse(s.substring(1, s.length - 1))
 }
 
 fun reverse(s: String): String {
