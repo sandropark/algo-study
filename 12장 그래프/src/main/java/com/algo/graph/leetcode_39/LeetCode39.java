4eedd84addbcabc19f336d623ded391d56bc5c13
sandropark <birdob@gmail.com>
2024-09-30 16:03:40 +0900

    [12장 그래프 - 39. 조합의 합] init

---
+++ 12장 그래프/src/main/java/com/algo/graph/leetcode_39/LeetCode39.java
@@ -0,0 +1,9 @@
+package com.algo.graph.leetcode_39;
+
+import java.util.List;
+
+public class LeetCode39 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return null;
+    }
+}
---
+++ 12장 그래프/src/test/java/com/algo/graph/leetcode_39/LeetCode39Test.java
@@ -0,0 +1,37 @@
+package com.algo.graph.leetcode_39;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LeetCode39Test {
+    @Test
+    void test() throws Exception {
+        // Given
+        int[] candidates = {2, 3, 6, 7};
+        int target = 7;
+        LeetCode39 sut = new LeetCode39();
+
+        // When
+        List<List<Integer>> result = sut.combinationSum(candidates, target);
+
+        // Then
+        assertThat(result).containsExactly(List.of(2, 2, 3), List.of(7));
+    }
+
+    @Test
+    void test2() throws Exception {
+        // Given
+        int[] candidates = {2, 3, 5};
+        int target = 8;
+        LeetCode39 sut = new LeetCode39();
+
+        // When
+        List<List<Integer>> result = sut.combinationSum(candidates, target);
+
+        // Then
+        assertThat(result).containsExactly(List.of(2, 2, 2, 2), List.of(2, 3, 3), List.of(3, 5));
+    }
+}
