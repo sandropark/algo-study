b43ed58b09f4c46db500e562dafa3f61e0997bd5
sandropark <birdob@gmail.com>
2024-09-30 16:36:49 +0900

    [12장 그래프 - 39. 조합의 합] 최적화
    - 5ms (10.30%), 45.48MB (6.02%)

--- 12장 그래프/src/main/java/com/algo/graph/leetcode_39/LeetCode39.java
+++ 12장 그래프/src/main/java/com/algo/graph/leetcode_39/LeetCode39.java
@@ -7,24 +7,22 @@ import java.util.List;
 public class LeetCode39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        dfs(candidates, target, 0, new ArrayList<>(), result);
+        dfs(candidates, target, 0, new ArrayList<>(), 0, result);
         return result;
     }
 
-    private static void dfs(int[] candidates, int target, int index, List<Integer> path, List<List<Integer>> result) {
-        int reduced = path.parallelStream().reduce(0, Integer::sum);
-
-        if (reduced == target) {
+    private static void dfs(int[] candidates, int target, int index, List<Integer> path, int tempResult, List<List<Integer>> result) {
+        if (tempResult == target) {
             result.add(new ArrayList<>(path));
             return;
         }
 
-        if (reduced > target) return;
+        if (tempResult > target) return;
 
         for (int i = 0; i < candidates.length; i++) {
             path.add(index, candidates[i]);
             int[] newCandidates = Arrays.copyOfRange(candidates, i, candidates.length);
-            dfs(newCandidates, target, index + 1, path, result);
+            dfs(newCandidates, target, index + 1, path, tempResult + candidates[i], result);
             path.remove(index);
         }
     }
