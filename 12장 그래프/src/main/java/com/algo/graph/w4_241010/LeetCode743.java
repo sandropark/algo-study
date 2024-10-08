bc205f7ce389672e9447fd86c002632a74f4cb92
sandropark <birdob@gmail.com>
2024-10-08 09:51:10 +0900

    [12장 그래프 - 743. 네트워크 딜레이 타임] BFS 임시저장

--- 12장 그래프/src/main/java/com/algo/graph/w4_241010/LeetCode743.java
+++ 12장 그래프/src/main/java/com/algo/graph/w4_241010/LeetCode743.java
@@ -1,7 +1,63 @@
 package com.algo.graph.w4_241010;
 
+import java.util.*;
+
 public class LeetCode743 {
     public int networkDelayTime(int[][] times, int n, int k) {
-        return 0;
+        int[] result = initResultList(n, k);
+        Map<Integer, List<int[]>> graph = makeGraph(times);
+        Queue<int[]> q = new ArrayDeque<>();
+
+        List<int[]> nodes = graph.get(k);
+        if (nodes != null)
+            q.addAll(nodes);
+
+        while (!q.isEmpty()) {
+            int[] v = q.poll();
+            int dest = v[1];
+            int cost = v[2];
+
+            int pastCost = result[dest];
+            result[dest] = pastCost == -1 ? cost : Math.min(pastCost, cost);
+
+            nodes = graph.get(dest);
+            if (nodes != null && !nodes.isEmpty()) {
+                nodes.forEach(ints -> ints[2] += cost);
+                q.addAll(nodes);
+                nodes.clear();
+            }
+        }
+
+        if (!allVisited(result)) return -1;
+
+        return getResult(result);
+    }
+
+    private static int getResult(int[] resultList) {
+        int result = 0;
+        for (int cost : resultList) result = Math.max(result, cost);
+        return result;
+    }
+
+    private static int[] initResultList(int n, int k) {
+        int[] resultList = new int[n + 1];
+        Arrays.fill(resultList, -1);
+        resultList[k] = 0;
+        resultList[0] = 0;
+        return resultList;
+    }
+
+    private boolean allVisited(int[] visited) {
+        for (int i : visited) if (i == -1) return false;
+        return true;
+    }
+
+    private static HashMap<Integer, List<int[]>> makeGraph(int[][] times) {
+        HashMap<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] time : times)
+            Optional.ofNullable(graph.get(time[0]))
+                    .ifPresentOrElse(values -> values.add(time),
+                            () -> graph.put(time[0], new ArrayList<>(Arrays.asList(time))));
+        return graph;
     }
 }
--- 12장 그래프/src/test/java/com/algo/graph/w4_241010/LeetCode743Test.java
+++ 12장 그래프/src/test/java/com/algo/graph/w4_241010/LeetCode743Test.java
@@ -48,4 +48,32 @@ class LeetCode743Test {
         // Then
         assertThat(result).isEqualTo(-1);
     }
+
+    @Test
+    void test4() throws Exception {
+        // Given
+        int[][] times = {{1, 2, 1}, {2, 1, 3}};
+        int n = 2;
+        int k = 2;
+
+        // When
+        int result = sut.networkDelayTime(times, n, k);
+
+        // Then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void test5() throws Exception {
+        // Given
+        int[][] times = {{4, 2, 76}, {1, 3, 79}, {3, 1, 81}, {4, 3, 30}, {2, 1, 47}, {1, 5, 61}, {1, 4, 99}, {3, 4, 68}, {3, 5, 46}, {4, 1, 6}, {5, 4, 7}, {5, 3, 44}, {4, 5, 19}, {2, 3, 13}, {3, 2, 18}, {1, 2, 0}, {5, 1, 25}, {2, 5, 58}, {2, 4, 77}, {5, 2, 74}};
+        int n = 5;
+        int k = 3;
+
+        // When
+        int result = sut.networkDelayTime(times, n, k);
+
+        // Then
+        assertThat(result).isEqualTo(59);
+    }
 }
