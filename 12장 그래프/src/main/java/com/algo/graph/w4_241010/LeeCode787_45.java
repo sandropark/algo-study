0018ca7aed7822861a311acc74bf9c3655bed70c
sandropark <ticoba@naver.com>
2024-10-09 12:42:49 +0900

    [12장 그래프 - 45.787. 저렴한 항공권] 1차 풀이
    - 17ms (9.60%), 47.10mb (5.41%)

--- 12장 그래프/src/main/java/com/algo/graph/w4_241010/LeeCode787_45.java
+++ 12장 그래프/src/main/java/com/algo/graph/w4_241010/LeeCode787_45.java
@@ -4,7 +4,15 @@ import java.util.*;
 
 public class LeeCode787_45 {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> graph = makeGraph(flights);
+        boolean hasFlightToDst = false;
+        HashMap<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] f : flights) {
+            if (f[1] == dst) hasFlightToDst = true;
+            graph.putIfAbsent(f[0], new ArrayList<>());
+            graph.get(f[0]).add(f);
+        }
+        if (!hasFlightToDst) return -1;
+
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(flight -> flight[1]));
         pq.offer(new int[]{src, 0, 0});
 
@@ -18,23 +26,13 @@ public class LeeCode787_45 {
 
             if (visitCount <= k) {
                 if (graph.containsKey(from)) {
-                    for (int[] nextFlight : graph.get(from)) {
-                        int newPrice = price + nextFlight[2];
-                        pq.offer(new int[]{nextFlight[1], newPrice, visitCount + 1});
+                    for (int[] route : graph.get(from)) {
+                        int newPrice = price + route[2];
+                        pq.offer(new int[]{route[1], newPrice, visitCount + 1});
                     }
                 }
             }
         }
-
         return -1;
-    }
-
-    private Map<Integer, List<int[]>> makeGraph(int[][] flights) {
-        HashMap<Integer, List<int[]>> graph = new HashMap<>();
-        for (int[] flight : flights) {
-            graph.putIfAbsent(flight[0], new ArrayList<>());
-            graph.get(flight[0]).add(flight);
-        }
-        return graph;
     }
 }
