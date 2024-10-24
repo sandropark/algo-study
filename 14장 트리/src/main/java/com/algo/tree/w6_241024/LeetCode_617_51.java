b446f7ee6094b2c5dd9777199ab806988e7ce1ba
sandropark <ticoba@naver.com>
2024-10-24 21:35:06 +0900

    [14장. 트리 - 51. 이진 트리 병합] 설명 추가

--- 14장 트리/src/main/java/com/algo/tree/w6_241024/LeetCode_617_51.java
+++ 14장 트리/src/main/java/com/algo/tree/w6_241024/LeetCode_617_51.java
@@ -4,9 +4,14 @@ import com.algo.tree.TreeNode;
 
 /**
  * 두 이진 트리의 병합
+ *
+ * 시간복잡도
+ *   각 트리 별로 최대 개수는 2000개다.
+ *   최대 2000개의 노드만 탐색하면 되기 때문에 O(n)이다.
  */
 public class LeetCode_617_51 {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        // 두 노드 중 하나라도 null이면 반환된다.
         if (root1 == null) return root2;
         if (root2 == null) return root1;
 
