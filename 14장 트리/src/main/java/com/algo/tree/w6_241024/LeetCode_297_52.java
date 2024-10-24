7005b55c01042f58c68d8bb32d8f3a5a6100b484
sandropark <ticoba@naver.com>
2024-10-24 21:24:15 +0900

    [14장. 트리 - 52.297. 이진 트리의 직렬화 & 역직렬화] 1차 풀이

--- 14장 트리/src/main/java/com/algo/tree/w6_241024/LeetCode_297_52.java
+++ 14장 트리/src/main/java/com/algo/tree/w6_241024/LeetCode_297_52.java
@@ -10,41 +10,54 @@ import java.util.LinkedList;
 public class LeetCode_297_52 {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        // 인덱스를 0부터 채우기위해서 추가
         StringBuilder sb = new StringBuilder("#,");
         LinkedList<TreeNode> q = new LinkedList<>();
-
         q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
 
+        while (!q.isEmpty()) {
+            TreeNode node = q.pop();
+            // 노드가 null인 경우, 자리수를 채우기 위해서 '#'를 넣고 다음 반복으로
             if (node == null) {
                 sb.append("#,");
                 continue;
             }
-
             sb.append(node.val).append(",");
             q.offer(node.left);
             q.offer(node.right);
         }
-
         return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         String[] split = data.split(",");
+
         String val = split[1];
         if (val.equals("#")) return null;
 
         TreeNode root = new TreeNode(Integer.parseInt(val));
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.add(root);
 
-        for (int i = 2; i < split.length; i++) {
-            String tempVal = split[i];
-            if (!tempVal.equals("#")) {
+        int index = 2;
+        while (!q.isEmpty()) {
+            TreeNode node = q.pop();
 
+            if (!split[index].equals("#")) {
+                node.left = new TreeNode(Integer.parseInt(split[index]));
+                q.add(node.left);
             }
-        }
 
+            index++;
+
+            if (!split[index].equals("#")) {
+                node.right = new TreeNode(Integer.parseInt(split[index]));
+                q.add(node.right);
+            }
+
+            index++;
+        }
 
         return root;
     }
