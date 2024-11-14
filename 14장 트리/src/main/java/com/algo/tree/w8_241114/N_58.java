cba2f2d11cee4ba922604173b93dcb7ca6e59126
sandropark <ticoba@naver.com>
2024-11-14 22:22:27 +0900

    [14장. 58]

--- 14장 트리/src/main/java/com/algo/tree/w8_241114/N_58.java
+++ 14장 트리/src/main/java/com/algo/tree/w8_241114/N_58.java
@@ -7,21 +7,22 @@ public class N_58 {
      * 시간복잡도
      * O(n) 다 순회하면 된다.
      */
-    private Integer prev = null; // 이전 노드의 값을 저장
-    private int minDiff = Integer.MAX_VALUE; // 최소 차이 초기화
+    Integer prev = null; // 이전 노드의 값을 저장
+    int minDiff = 100_001; // 최소 차이 초기화. 최소 값 0, 최대 값이 100,000
 
     public int minDiffInBST(TreeNode root) {
-        inOrderTraversal(root);
+        inOrderTraversal(root);  // 중위순회
         return minDiff;
     }
 
-    private void inOrderTraversal(TreeNode node) {
+    void inOrderTraversal(TreeNode node) {
         if (node == null) return;
 
+        // 왼쪽 서브트리 탐색
         inOrderTraversal(node.left);
 
-        // 현재 노드와 이전 노드 간의 차이 계산
         if (prev != null)
+            // 현재 노드와 이전 노드 간의 차이 계산
             minDiff = Math.min(minDiff, node.val - prev);
 
         prev = node.val; // 현재 노드 값을 이전 노드로 저장
