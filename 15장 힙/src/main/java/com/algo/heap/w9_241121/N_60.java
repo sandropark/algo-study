108673a118d762b5c7a93943612a9c65bbc1eda5
sandropark <ticoba@naver.com>
2024-11-21 22:13:46 +0900

    [15장 힙 - 60] 설명 추가

--- 15장 힙/src/main/java/com/algo/heap/w9_241121/N_60.java
+++ 15장 힙/src/main/java/com/algo/heap/w9_241121/N_60.java
@@ -2,6 +2,19 @@ package com.algo.heap.w9_241121;
 
 import java.util.PriorityQueue;
 
+/*
+60. 릿코드 215. Kth Largest Element in an Array
+
+Constraints:
+
+1 <= k <= nums.length <= 10^5 (100k)
+    - n^2은 안 됨
+-10^4 <= nums[i] <= 10^4
+    - 큰 상관 없음
+
+시간 복잡도 : O(n log k)
+공간 복잡도 : O(k)
+ */
 public class N_60 {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
