5fec40143b2c01579d88303efce78aa99270e5ea
sandropark <birdob@gmail.com>
2024-11-21 12:48:36 +0900

    [15장 힙 - 60] 3차 풀이 - 최적화

--- 15장 힙/src/main/java/com/algo/heap/w9_241121/N_60.java
+++ 15장 힙/src/main/java/com/algo/heap/w9_241121/N_60.java
@@ -1,19 +1,17 @@
 package com.algo.heap.w9_241121;
 
-import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class N_60 {
     public int findKthLargest(int[] nums, int k) {
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        for (int num : nums)
-            pq.add(num);
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k)
+                minHeap.poll();
+        }
 
-        for (int i = 0; i < k - 1; i++)
-            pq.poll();
-
-        return pq.poll();
+        return minHeap.poll();
     }
 }
