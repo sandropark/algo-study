c272db301078fd23f2f374c5b78cb71ca4248a68
sandropark <ticoba@naver.com>
2024-11-26 21:28:39 +0900

    [16장 트라이 - 336] 비교 로직 수정

--- 16장 트라이/src/main/java/com/algo/trie/w10_241128/M_63.java
+++ 16장 트라이/src/main/java/com/algo/trie/w10_241128/M_63.java
@@ -9,24 +9,13 @@ public class M_63 {
 
         for (int i = 0; i < words.length; i++)
             for (int j = 0; j < words.length; j++)
-                if (i != j && palindromePairs(words[i] + words[j]))
+                if (i != j && isPalindromePairs(words[i] + words[j]))
                     result.add(List.of(i, j));
 
         return result;
     }
 
-    private boolean palindromePairs(String newWord) {
-        int length = newWord.length();
-        if (length < 2) return true;
-
-        int start = 0, end = length - 1;
-        char s, e;
-        while (start < end) {
-            s = newWord.charAt(start++);
-            e = newWord.charAt(end--);
-            if (s != e)
-                return false;
-        }
-        return true;
+    private boolean isPalindromePairs(String newWord) {
+        return newWord.contentEquals(new StringBuilder(newWord).reverse());
     }
 }
